49918d6e1686c6ff80b88316dc54cf8c0b53a2e6
Gabriel Montalvo <gabe.montalvo5@gmail.com>
2015-09-05 18:49:05 -0400

    doc js file

--- js/main.js
+++ js/main.js
@@ -1,61 +1,67 @@
 function disBtn(btn) {
-    btn.disabled = true;
+    btn.disabled = true; // disable button method
 }
 
 function enabBtn(EncBtn) {
-    EncBtn.disabled = false;
+    EncBtn.disabled = false; // enable button method
 }
 
-function cut(str, cutStart, cutEnd){
+function cut(str, cutStart, cutEnd){ // custom function to cut strings
   return str.substr(0,cutStart) + str.substr(cutEnd+1);
 }
 
-function clearMsg() {
-    document.getElementById("usrInput").value = "";
+function clearMsg() { // clears textarea value
+    document.getElementById("txtbox").value = "";
 }
 
+// GENERATE NEW KEY PAIR //
 function generate_key() {
 
-var options = {
-    numBits: 2048,
-    userId: 'User Name <usr@someserver.com>',
+var options = {     // generic data for demo purposes
+    numBits: 2048, 
+    userId: 'User Name <usr@someserver.com>', 
     passphrase: '123qweasdhgv34ghv'
 };
 
-     sessionStorage.setItem("passphrase", options.passphrase);
+     sessionStorage.setItem("passphrase", options.passphrase); // temporary local storage for passphrase  
 
-     window.openpgp.generateKeyPair(options).then(function(keyPair) {
+     window.openpgp.generateKeyPair(options).then(function(keyPair) { // openpgp key gen function
          
       // window.prompt("Your public key:", keyPair.publicKeyArmored);
-      console.log(keyPair.publicKeyArmored);
+      console.log(keyPair.publicKeyArmored); // displays public key block in console
       
+      // temporarily stores user's public and private key 
       sessionStorage.setItem("pubKeyString", keyPair.publicKeyArmored);
       sessionStorage.setItem("privKeyString", keyPair.privateKeyArmored);
      });
-				
+     
+// status change			
 document.getElementById("keyStat").className = "label label-primary";
 document.getElementById("keyStat").innerHTML = "A key has been generated for you";
 
 }
 
+// ENCRYPTION METHOD //
 function encryptMsg() {
-
+    // public key instance
     var publicKey = window.openpgp.key.readArmored(sessionStorage.getItem("pubKeyString"));
     
-    var raw_message = document.getElementById("usrInput").value;
+    // stores textarea value
+    var raw_message = document.getElementById("txtbox").value;
     
     window.openpgp.encryptMessage(publicKey.keys, raw_message).then(function(pgpMessage) {
         
-        sessionStorage.setItem("message", pgpMessage);
+        sessionStorage.setItem("message", pgpMessage);  
         
         pgpMessage = pgpMessage.replace("-----END PGP MESSAGE-----", " ");
         
-        document.getElementById("usrInput").value = cut(pgpMessage, 0, 88);
+        document.getElementById("txtbox").value = cut(pgpMessage, 0, 88); // display encrypted message
     });
 }   
 
+// DECRYPTION METHOD //
 function decryptMsg() {
-    
+    // private key instance
     var privateKey = window.openpgp.key.readArmored(sessionStorage.getItem("privKeyString")).keys[0];
     var passphrase = sessionStorage.getItem("passphrase");
     
@@ -65,6 +71,6 @@ function decryptMsg() {
     pgpMessage = window.openpgp.message.readArmored(pgpMessage);
     
     window.openpgp.decryptMessage(privateKey, pgpMessage).then(function(plaintext) {
-    document.getElementById("usrInput").value = plaintext;
+    document.getElementById("txtbox").value = plaintext; // display plain text data
     });
 }
